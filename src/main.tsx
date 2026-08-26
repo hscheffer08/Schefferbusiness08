458e69bff7afc95c2c4076cc8b798767db60e063
hscheffer08 <hscheffer08@gmail.com>
2026-08-26 10:10:33 -0300

    Enable English mode for US experience

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
+import UsEnglishMode from './lib/us-english-mode.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <UsEnglishMode />
     <Analytics />
   </StrictMode>
 );
