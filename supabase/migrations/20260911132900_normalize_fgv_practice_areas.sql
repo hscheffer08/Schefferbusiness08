a57e2abc429ca7b9b6d9cf4e599fa24eec6ae5c4
hscheffer08 <hscheffer08@gmail.com>
2026-09-11 10:23:47 -0300

    Normalize FGV practice area labels

---
+++ supabase/migrations/20260911132900_normalize_fgv_practice_areas.sql
@@ -0,0 +1,7 @@
+update public.exam_practice_questions
+set area='Língua Portuguesa'
+where active=true and exam_id='fgv' and area='Português';
+
+update public.exam_practice_questions
+set area='Ciências Humanas'
+where active=true and exam_id='fgv' and area='Humanas';
